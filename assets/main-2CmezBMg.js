29a7e4bd001584a5e5284e5d80e0db0d744d2edf
hhissa <hassan.h.eissa03@gmail.com>
2025-12-25 20:29:39 -0500

    Updates

---
+++ assets/main-2CmezBMg.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";const s="/CHECK_YOUR_FORESTS/";console.log("main.ts loaded");window.location.assign(`${s}src/pages/splash_screen.html`);
--- assets/main-DVtTUAP4.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";const o="/CHECK_YOUR_FORESTS/";console.log("main.ts loaded");window.location.assign(`${o}pages/splash_screen.html`);
